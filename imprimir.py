2ef710ec1b9ef7d920be17d2440927fe0af1aac9
puri0074 <1472315@senati.pe>
2023-03-04 10:37:35 -0500

    mi proyecto

--- imprimir.py
+++ imprimir.py
@@ -1,4 +1,6 @@
 # escriba un codigo de programacion donde me permite imprimir y ultimo valor de la lista:
-list-["cat", "dog","donkey", "cow"]
-valor_1 - list[0]
-valor_2 - list[-1]
+list=["cat", "dog","donkey", "cow"]
+valor_1 = list[0]
+valor_2 = list[-1]
+print("el primer valor de la lista es:", valor_1)
+print("el ultimo valor de la lista es:", valor_2)
